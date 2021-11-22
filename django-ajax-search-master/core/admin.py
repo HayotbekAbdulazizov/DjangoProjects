4bcbc77ca4c3283ea34e5b4793fa03fd1b5c53fe
Hayotbek2005 <hayotbekabdulazizov2005@gmail.com>
2021-11-22 23:58:35 +0500

    dele

--- django-ajax-search-master/core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from core.models import Artist, MusicRelease
-
-# Register your models here.
-
-admin.site.register(Artist)
-admin.site.register(MusicRelease)
--- django-ajax-search-master/django-ajax-search/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from core import views as core_views
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("artists/", core_views.artists_view, name="artists"),
-]
